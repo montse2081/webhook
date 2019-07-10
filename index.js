1a53c770bb04b683d0778997f24fc518c15bd29a
montse2081 <52506559+montse2081@users.noreply.github.com>
2019-07-10 11:48:53 -0500

    Update index.js

--- index.js
+++ index.js
@@ -15,11 +15,7 @@ restService.use(bodyParser.json());
 
 restService.post("/echo", function(req, res) {
   var speech =
-    req.body.queryResult &&
-    req.body.queryResult.parameters &&
-    req.body.queryResult.parameters.echoText
-      ? req.body.queryResult.parameters.echoText
-      : "Seems like some problem. Speak again."+req.body;
+   req.body;
   return res.json({
 
   "fulfillmentText": speech,
