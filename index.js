97c7312f3bb441b4ce21cabac5e2bc95a44bf860
montse2081 <52506559+montse2081@users.noreply.github.com>
2019-07-12 17:44:49 -0500

    Update index.js

--- index.js
+++ index.js
@@ -19,13 +19,15 @@ restService.post("/echo", function(req, res) {
    req.body.short_id;
   var charge = req.body.type;
   var result = req.body.id;
+  var status ="";
+	try{
   switch (charge){
       case "charge.success":
-		 
+		 status = "success";
             result = "pagado";
             break;
         case "charge.pending":
-		
+		status = "pending";
 		  const https = require('https')
 
 const data = JSON.stringify({
@@ -59,21 +61,20 @@ req.on('error', (error) => {
 
 req.write(data)
 req.end()
-		  
-		  
-		  
-		  
-     
             //result = "pending";
             break;
         case "charge.expired":
-		
+		status = "expired";
             result = "expirado";
             break;
       default:
       result = "en espera";
+		  status = "error";
   }
-  
+  }catch(e){
+	  result = e;
+		  status = e;
+  }
   return res.json({
 
   "status": "success",
