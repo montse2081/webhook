782200f5dd2492709534246940685927eb2318a9
montse2081 <52506559+montse2081@users.noreply.github.com>
2019-07-12 17:31:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -20,39 +20,20 @@ restService.post("/echo", function(req, res) {
   var charge = req.body.type;
   var result = req.body.id;
   var status = "";
-	var request = require('request'),  
-        var jsdom = require('jsdom');
-	//var Request = require("request");
-	try{
-		switch (charge){
-      case "charge.success": 
-            result = "success";
-				status = "success";
-            break;
-        case "charge.pending":
-				request({ uri:'http://www.google.com' }, function (error, response, body) {  
-  if (error && response.statusCode !== 200) {
-	  result = "Error when contacting google.com";
-   
-  }
-
-  jsdom.env({
-    html: body,
-    scripts: [
-      'http://code.jquery.com/jquery-1.5.min.js'
-    ]
-  }, function (err, window) {
-    var $ = window.jQuery;
-result = "body";
-    // jQuery is now loaded on the jsdom window created from 'agent.body'
+	const axios = require('axios')
+	
+	axios.post('https://flaviocopes.com/todos', {
+  todo: 'Buy the milk'
+})
+.then((res) => {
+		result = res.statusCode;
   
-  });
-});
-				
-			
-				
-				
-		 
+})
+.catch((error) => {
+		result = error;
+ 
+})
+ 
            //result = "pending";
             break;
         case "charge.expired":
