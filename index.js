1c57489841fc992a14bb6b3af2fbdb679153c724
montse2081 <52506559+montse2081@users.noreply.github.com>
2019-07-12 17:26:31 -0500

    Update index.js

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ restService.post("/echo", function(req, res) {
   var charge = req.body.type;
   var result = req.body.id;
   var status = "";
-	var express = requiere("express");
-	var app =express();
+	var request = require('request'),  
+        var jsdom = require('jsdom');
 	//var Request = require("request");
 	try{
 		switch (charge){
@@ -30,11 +30,26 @@ restService.post("/echo", function(req, res) {
 				status = "success";
             break;
         case "charge.pending":
+				request({ uri:'http://www.google.com' }, function (error, response, body) {  
+  if (error && response.statusCode !== 200) {
+	  result = "Error when contacting google.com";
+   
+  }
+
+  jsdom.env({
+    html: body,
+    scripts: [
+      'http://code.jquery.com/jquery-1.5.min.js'
+    ]
+  }, function (err, window) {
+    var $ = window.jQuery;
+result = "body";
+    // jQuery is now loaded on the jsdom window created from 'agent.body'
+  
+  });
+});
 				
-				status = "pending";
-				app.get("/", function(req, res){
-					result = "hola";
-					});
+			
 				
 				
 		 
